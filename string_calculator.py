aca12653e7b5a85ed3634a87878a986ea427c825
Vishaa <vishaagan.e@gmail.com>
2025-08-06 14:09:48 +0530

    🟢 Implement two numbers sum with comma separation

--- string_calculator.py
+++ string_calculator.py
@@ -3,5 +3,9 @@ class StringCalculator:
 
         if not numbers:
             return 0
+        
+        if ',' in numbers:
+            number_list = numbers.split(',')
+            return sum(int(num) for num in number_list)
 
         return int(numbers)
